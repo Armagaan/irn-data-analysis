44e52d0737afb4d295b4cfe919467402ca18a827
Manthan Dalmia <78015740+ManthanDalmia@users.noreply.github.com>
2023-11-15 00:00:55 +0530

    scrapping added

---
+++ scrapping/func.py
@@ -0,0 +1,13 @@
+def getTrainInfo(trainNumber)
+
+def getDelay(trainNumber, dateRange)
+
+def getAvgDelay(trainNumber, dateRange)
+
+def getStationDelay(stationCode, dateRange)
+
+def getAvgStationDelay(stationCode, dateRange)
+
+def predictStationDelay(stationCode, date)
+
+. . . and more
